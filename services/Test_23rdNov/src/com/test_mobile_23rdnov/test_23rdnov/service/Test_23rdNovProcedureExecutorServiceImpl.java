caee0eac6fb26fd25e76223d25bc655f56efbba3
Surya null <surya.talluri@gmail.com>
2015-11-23 09:03:27 +0000

    New files added - [Table5Service.java,Test_sort.properties,Table2Dao.java,ui-bg_glass_95_fef1ec_1x400.png,Test_sort-procedures.mappings.json,Test_sort_API.json,Table1Id.java,Table5Dao.java,ui-icons_777777_256x240.png,Test_sortQueryExecutorServiceImpl.java,Table2Controller.java,Test_23rdNov_published_dataModel.json,db-connection-settings.json,Test_23rdNov_query.json,Table5Controller.java,FontAwesome.otf,Table1.java,fontawesome-webfont.woff,Table2Service.java,Test_23rdNovQueryExecutorService.java,ui-icons_777620_256x240.png,Test_23rdNov-procedures.mappings.json,Test_23rdNov_procedure.json,ui-bg_glass_75_e6e6e6_1x400.png,glyphicons-halflings-regular.woff,Test_sort_vk.hbm.xml,Table1Service.java,Test_23rdNovProcedureExecutorService.java,ui-icons.png,Test_23rdNov.spring.xml,Table2Id.java,ProcedureExecutionController.java,spinner-small.gif,fontawesome-webfont.svg,ui-icons_2e83ff_256x240.png,Test_sortProcedureExecutorServiceImpl.java,QueryExecutionController.java,ui-bg_highlight-soft_75_cccccc_1x100.png,Test_sort.spring.xml,Table2ServiceImpl.java,wm-mobileloader.min.js,Table1Dao.java,ui-bg_glass_65_ffffff_1x400.png,Table1Id.java,Table1Controller.java,glyphicons-halflings-regular.woff2,Test_sort_query.json,Table2ServiceImpl.java,wm-style.css,ui-icons_444444_256x240.png,Test_sort_procedure.json,Table5ServiceImpl.java,glyphicons-halflings-regular.ttf,Table1Dao.java,ui-icons_888888_256x240.png,Test_sortQueryExecutorService.java,db-connection-settings.json,glyphicons-halflings-regular.eot,fontawesome-webfont.eot,ui-bg_flat_75_ffffff_40x100.png,Test_sortProcedureExecutorService.java,Table1ServiceImpl.java,Table2.java,Table2Id.java,Table1ServiceImpl.java,Test_sort_published_dataModel.json,Test_23rdNovQueryExecutorServiceImpl.java,Test_23rdNov.properties,wm-libs.min.js,Test_23rdNov_vk.hbm.xml,Test_23rdNovProcedureExecutorServiceImpl.java,glyphicons-halflings-regular.svg,bootstrap.css.map,Table5Id.java,Table1Service.java,ui-icons_555555_256x240.png,Table1.java,Table2Controller.java,ui-icons_222222_256x240.png,Table5.java,ui-icons_cc0000_256x240.png,ui-bg_flat_0_aaaaaa_40x100.png,Test_23rdNov-queries.hbm.xml,ui-icons_cd0a0a_256x240.png,servicedef.xml,Table2Service.java,QueryExecutionController.java,ui-bg_glass_55_fbf9ee_1x400.png,Table2.java,Table1Controller.java,ProcedureExecutionController.java,fontawesome-webfont.ttf,Table2Dao.java,Test_23rdNov.hbm.xml,Test_23rdNov_API.json,servicedef.xml,fontawesome-webfont.woff2,loader.gif,ui-icons_ffffff_256x240.png,ui-bg_glass_75_dadada_1x400.png,Test_sort-queries.hbm.xml,ui-icons_454545_256x240.png] Files modified - [Main.html,login.html,project-services.xml,Main.css,wmProperties.js,app.variables.json,Main.variables.json,production.properties,Main.js,pom.xml,index.html,page.min.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/Test_23rdNov/src/com/test_mobile_23rdnov/test_23rdnov/service/Test_23rdNovProcedureExecutorServiceImpl.java
@@ -0,0 +1,43 @@
+/*Copyright (c) 2015-2016 gmail.com All Rights Reserved.
+ This software is the confidential and proprietary information of gmail.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with gmail.com*/
+
+
+package com.test_mobile_23rdnov.test_23rdnov.service;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.dao.procedure.WMProcedureExecutor;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+@Service
+public class Test_23rdNovProcedureExecutorServiceImpl implements Test_23rdNovProcedureExecutorService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Test_23rdNovProcedureExecutorServiceImpl.class);
+
+	@Autowired
+	@Qualifier("Test_23rdNovWMProcedureExecutor")
+	private WMProcedureExecutor procedureExecutor;
+
+
+	@Transactional(value = "Test_23rdNovTransactionManager")
+	@Override
+	public List<Object> executeWMCustomProcedure(CustomProcedure procedure) {
+	    return procedureExecutor.executeCustomProcedure(procedure);
+	}
+
+
+}
+
---
+++ services/Test_sort/src/com/test_mobile_23rdnov/test_sort/service/Test_sortProcedureExecutorService.java
@@ -0,0 +1,22 @@
+/*Copyright (c) 2015-2016 gmail.com All Rights Reserved.
+ This software is the confidential and proprietary information of gmail.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with gmail.com*/
+
+
+package com.test_mobile_23rdnov.test_sort.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface Test_sortProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
